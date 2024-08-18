2162ba5348d3a24dfb561bbf8859a223a743e5bf
CYIZA Landry MPAYIMANA <103103265+CyizaLandry5@users.noreply.github.com>
2024-08-18 14:28:55 +0200

    Float_Substraction

---
+++ Day1_DynamicAdditionWithFloatVariable.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+
+	float a,b,result;
+
+	//Dynamic program
+	printf("Enter the first value");
+	scanf("%f", & a);
+	printf("Enter the second value");
+	scanf("%f", & b);
+	
+	result= a+b;
+	
+	printf("Result is: %d" ,result);
+	return 0;
+	
+}
