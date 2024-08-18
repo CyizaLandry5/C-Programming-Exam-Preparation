b84d5faf568a8cfcc2b4622f5ef70a22b8cbd866
CYIZA Landry MPAYIMANA <103103265+CyizaLandry5@users.noreply.github.com>
2024-08-18 14:19:42 +0200

    Names 10Times
    No Loop

---
+++ Day1_ExerciseMyNamesTenTimes.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+const char* name ="MPAYIMANA CYIZA Landry";
+printf("%s\n", name);
+printf("%s\n", name);
+printf("%s\n", name);
+printf("%s\n", name);
+printf("%s\n", name);
+printf("%s\n", name);
+printf("%s\n", name);
+printf("%s\n", name);
+printf("%s\n", name);
+printf("%s\n", name);
+//	printf("1.MPAYIMANA CYIZA Landry\n");
+//	printf("2.MPAYIMANA CYIZA Landry\n");
+//	printf("3.MPAYIMANA CYIZA Landry\n");
+//	printf("4.MPAYIMANA CYIZA Landry\n");
+//	printf("5.MPAYIMANA CYIZA Landry\n");
+//	printf("6.MPAYIMANA CYIZA Landry\n");
+//	printf("7.MPAYIMANA CYIZA Landry\n");
+//	printf("8.MPAYIMANA CYIZA Landry\n");
+//	printf("9.MPAYIMANA CYIZA Landry\n");
+//	printf("10.MPAYIMANA CYIZA Landry\n");
+	return 0;
+}
