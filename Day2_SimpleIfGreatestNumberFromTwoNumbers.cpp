6bc868f42eccfa690870988b8201c76ce53daec1
CYIZA Landry MPAYIMANA <103103265+CyizaLandry5@users.noreply.github.com>
2024-08-19 11:29:55 +0200

    SimpleIfGreatestNumberFromTwoNumbers

---
+++ Day2_SimpleIfGreatestNumberFromTwoNumbers.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int n1,n2;
+	printf("Enter value of N1");
+	scanf("%d", &n1);
+	printf("Enter value of N2");
+	scanf("%d", &n2);
+	
+	if(n1>n2)
+		printf("N1 is the greatest");
+		
+		else
+			printf("N2 is the greatest");
+			
+	return 0;
+
+}
