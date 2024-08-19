94c0032610986a768f10387f05b124fee052c18f
CYIZA Landry MPAYIMANA <103103265+CyizaLandry5@users.noreply.github.com>
2024-08-19 13:36:55 +0200

    1OddOrEvenProgram

---
+++ Day2_1OddOrEvenProgram.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter any number to chech whether it is odd or even");
+	scanf("%d", &a);
+	
+	if (a%2!=0)
+	{
+		printf("Number is Odd");
+	}
+	else
+	{
+		printf("Number is Even");
+	}
+return 0;
+	
+}
