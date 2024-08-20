76771ac113bb0342e79f4eb4e6a241ae85b84186
CYIZA Landry MPAYIMANA <103103265+CyizaLandry5@users.noreply.github.com>
2024-08-20 08:02:08 +0200

    InitialFunctionApp

---
+++ Day3_From1to100WithForAndMessage.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int value;
+	for(value=1; value<=100; value++)
+	{
+		printf("The value is %d\n" ,value);
+	}
+	return 0;
+}
---
+++ Day3_FunctionToSetNamesAndAge.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void ide(char name[], int age)
+{
+	printf("Name : %s , Age = %d",name,age );
+}
+main(){
+	
+ide("Uwera", 19);
+}
+
